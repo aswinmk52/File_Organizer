c53b6152ac1c62ddfbd9fb1963289fc5ce3e1e21
Aswin <aswinmk52@gmail.com>
2025-01-08 14:52:16 +0000

    Added some more extensions to the dictionary. Added blank line seperating the code and comments

--- file_organizer.py
+++ file_organizer.py
@@ -3,7 +3,7 @@
 import os
 
 # Categorizing each file extensions to a dictionary
-file_categories = {'.txt':'Documents', '.doc':'Documents', '.docx':'Documents', '.pdf':'Documents', '.jpg':'Images', '.jpeg':'Images', '.png':'Images', '.mp3':'Musics', '.wav':'Musics', '.mp4':'Videos'}
+file_categories = {'.txt':'Documents', '.doc':'Documents', '.docx':'Documents', '.pdf':'Documents', '.xlsx':'Documents', '.jpg':'Images', '.jpeg':'Images', '.png':'Images', '.mp3':'Musics', '.wav':'Musics', '.mp4':'Videos', '.mkv':'Videos', '.psd':'Photoshop_Files', '.exe':'Applications', '.apk':'Applications', '.html':'HTML_Files', '.zip':'Compressed_Files'}
 
 # Get source directory from the user
 source_directory = input("Enter the path to the source directory")
@@ -12,11 +12,14 @@ source_directory = input("Enter the path to the source directory")
 for filename in os.listdir(source_directory):
     # Get the file extension
     file_extension = os.path.splitext(filename)[1].lower()
+
     # Determine the category
     category = file_categories.get(file_extension, 'Others')
+
     # Create a category path does not exists
     category_path = os.path.join(source_directory, category)
     os.makedirs(category_path, exist_ok = True)
+
     # Move the file to appropriate category folder
     source_path = os.path.join(source_directory, filename)
     destination_path = os.path.join(category_path, filename)
